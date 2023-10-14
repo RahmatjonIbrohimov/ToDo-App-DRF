f97021a750bf0eff7f89fe257be81839e3841afe
Rahmatjon <128359200+RahmatjonIbrohimov@users.noreply.github.com>
2023-10-14 15:24:59 +0500

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeApiViews.as_view(), name='home'),
+    path('all/', views.HomeApiViews.as_view(), name='home'),
     path('<int:pk>/', views.TDetailApiViews.as_view(), name='detail-page'),
     path('<int:pk>/update/', views.TUpdateApiViews.as_view(), name='update'),
     path('new/', views.TCreateApiViews.as_view(), name='create-page'),
